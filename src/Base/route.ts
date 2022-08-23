9f18f65cccc5300db9ffd551175244cc11af5784
aslkami <aslkami@yahoo.com>
2022-08-23 23:25:36 +0800

    网络

--- src/Base/route.ts
+++ src/Base/route.ts
@@ -1,27 +1,27 @@
 export default {
   navs: [
     {
-      title: "基础",
-      path: '/base'
-    }
+      title: '基础',
+      path: '/base',
+    },
   ],
   menus: {
     '/base': [
       {
         title: '原型及原型链',
-        path: '/base/prototype'
+        path: '/base/prototype',
       },
       {
         title: 'New 的模拟实现',
-        path: '/base/new'
+        path: '/base/new',
       },
       {
         title: 'call 和 apply',
-        path: '/base/call-apply'
+        path: '/base/call-apply',
       },
       {
         title: 'bind',
-        path: '/base/bind'
+        path: '/base/bind',
       },
       {
         title: '防抖',
@@ -30,7 +30,11 @@ export default {
       {
         title: '节流',
         path: '/base/throttle',
-      }
-    ]
-  }
-}
+      },
+      {
+        title: '网络',
+        path: '/base/network',
+      },
+    ],
+  },
+};
